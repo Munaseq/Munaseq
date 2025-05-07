4d6b2d2f283c3a2a26da7f23303409ce816d07d5
mohdAlmahmud <mohd.almahmud@gmail.com>
2025-05-07 09:56:39 +0300

    Munaseq is now 0.95

--- frontend/src/app/(authenticated-content)/event/[eventId]/(event-layout)/about/page.tsx
+++ frontend/src/app/(authenticated-content)/event/[eventId]/(event-layout)/about/page.tsx
@@ -5,79 +5,79 @@ import Image from "next/image";
 import { UserDataDto } from "@/dtos/user-data.dto";
 import { notFound } from "next/navigation";
 import {
-    CalendarDaysIcon,
-    MapPinIcon,
-    UserRoundIcon,
-    UsersRoundIcon,
+  CalendarDaysIcon,
+  MapPinIcon,
+  UserRoundIcon,
+  UsersRoundIcon,
 } from "lucide-react";
+import ReminderSelect from "@/components/authenticated-content/event/event-layout/reminder-select";
 
 export default async function AboutPage({
-    params,
+  params,
 }: {
-    params: { eventId: string };
+  params: { eventId: string };
 }) {
-    const event: EventDataDto = await getEventAction(params.eventId);
-    if (!event) {
-        notFound();
-    }
-    const user: UserDataDto = event.eventCreator;
+  const event: EventDataDto = await getEventAction(params.eventId);
+  if (!event) {
+    notFound();
+  }
+  const user: UserDataDto = event.eventCreator;
 
-    return (
-        <div className="">
-            <div className="flex relative md:flex-row flex-col"></div>
-            <div className="">
-                <div className="mb-5 flex flex-wrap gap-2">
-                    {event.categories.map((category: string) => {
-                        return <Category key={category}>{category}</Category>;
-                    })}
-                </div>
-                <div className="grid gap-3">
-                    <h1 className="font-bold text-3xl">{event.title}</h1>
-                    <div className="flex items-center text-custom-light-purple">
-                        <UserRoundIcon />
-                        <div className=" font-semibold text-xl">
-                            <p>
-                                {"المنسق " +
-                                    user.firstName +
-                                    " " +
-                                    user.lastName}
-                            </p>
-                        </div>
-                    </div>
-                    <div className="text-custom-black">{event.description}</div>
-                </div>
-            </div>
-            <div className="mt-10">
-                <h1 className="font-bold ">معلومات الحضور</h1>
-                <div className="text-custom-black gap-1 flex flex-col">
-                    <p className="flex gap-2 items-center">
-                        <CalendarDaysIcon />
-                        {"تاريخ بدأ الفعالية: " +
-                            new Date(event.startDateTime).toLocaleDateString()}
-                    </p>
-                    <p className="flex gap-2 items-center">
-                        <CalendarDaysIcon />
-                        {"تاريخ انتهاء الفعالية: " +
-                            new Date(event.endDateTime).toLocaleDateString()}
-                    </p>
-                    <p className="flex gap-2 items-center">
-                        <UsersRoundIcon />
-                        {(!event.isOnline ? "حضوري" : "عن بعد") +
-                            " " +
-                            (event.gender === "BOTH"
-                                ? "للجميع"
-                                : event.gender === "MALE"
-                                ? "للذكور"
-                                : "للإناث")}
-                    </p>
-                    {!event.isOnline && (
-                        <p className="flex gap-2 items-center">
-                            <MapPinIcon />
-                            {event.location}
-                        </p>
-                    )}
-                </div>
-            </div>
+  return (
+    <div className="">
+      <div className="flex relative md:flex-row flex-col"></div>
+      <div className="">
+        <div className="mb-5 flex flex-wrap gap-2">
+          {event.categories.map((category: string) => {
+            return <Category key={category}>{category}</Category>;
+          })}
         </div>
-    );
+        <div className="grid gap-3">
+          <h1 className="font-bold text-3xl">{event.title}</h1>
+          <div className="flex items-center text-custom-light-purple">
+            <UserRoundIcon />
+            <div className=" font-semibold text-xl">
+              <p>{"المنسق " + user.firstName + " " + user.lastName}</p>
+            </div>
+          </div>
+          <div className="text-custom-black">{event.description}</div>
+        </div>
+      </div>
+      <div className="mt-10">
+        <h1 className="font-bold ">معلومات الحضور</h1>
+        <div className="text-custom-black gap-1 flex flex-col">
+          <p className="flex gap-2 items-center">
+            <CalendarDaysIcon />
+            {"تاريخ بدأ الفعالية: " +
+              new Date(event.startDateTime).toLocaleDateString()}
+          </p>
+          <p className="flex gap-2 items-center">
+            <CalendarDaysIcon />
+            {"تاريخ انتهاء الفعالية: " +
+              new Date(event.endDateTime).toLocaleDateString()}
+          </p>
+          <p className="flex gap-2 items-center">
+            <UsersRoundIcon />
+            {(!event.isOnline ? "حضوري" : "عن بعد") +
+              " " +
+              (event.gender === "BOTH"
+                ? "للجميع"
+                : event.gender === "MALE"
+                ? "للذكور"
+                : "للإناث")}
+          </p>
+          {!event.isOnline && (
+            <p className="flex gap-2 items-center">
+              <MapPinIcon />
+              {event.location}
+            </p>
+          )}
+        </div>
+        <ReminderSelect
+          startDate={event.startDateTime}
+          eventId={params.eventId}
+        />
+      </div>
+    </div>
+  );
 }
