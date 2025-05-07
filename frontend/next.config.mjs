7b1253a6ad80580808a5fb307011518061e4bd14
Krcherheadx <hishamxbox@gmail.com>
2025-05-07 18:05:32 +0300

    FrontEnd: Adding the new AWS S3 domain

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["munaseq.s3.eu-north-1.amazonaws.com"],
+    domains: [
+      "munaseq.s3.eu-north-1.amazonaws.com",
+      "prod-munaseq.s3.me-south-1.amazonaws.com",
+    ],
   },
 };
 
