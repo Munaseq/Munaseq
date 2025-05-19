164d62522eda17a958b5e42455560bd0a2e4f5a6
Hussam Qannam <hussam.a.qannam@gmail.com>
2025-05-19 16:39:01 +0300

    updated next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,8 +2,7 @@
 const nextConfig = {
   images: {
     domains: [
-      "munaseq.s3.eu-north-1.amazonaws.com",
-      "prod-munaseq.s3.me-south-1.amazonaws.com",
+      `${process.env.S3_BUCKET}`
     ],
   },
 };
